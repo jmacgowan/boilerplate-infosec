15f95a0261b378a45d1d41891b7ca664d1eae118
Joseph Macgowan <macgowan.joseph@gmail.com>
2024-02-20 14:22:53 +0000

    Helmet part1

--- myApp.js
+++ myApp.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
-
-
-
+/*
+The following lines are whats included, can be added or removed. 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+const timeInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+*/
+app.use(helmet());
+//These are not in be default
+/*const directives = {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'", 'trusted-cdn.com']
+};
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives}));*/
 
 
 
